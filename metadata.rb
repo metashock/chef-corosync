5e3be432f7a91143631268c9c831b4d27423aaf7
Thorsten Heymann <info@metashock.net>
2014-10-11 11:42:26 +0200

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'corosync'
+maintainer       'Thorsten Heymann'
+maintainer_email 'hek2mgl@metashock.net'
+license          'MIT'
+description      'Installs/Configures corosync'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
